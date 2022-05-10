8661c8fed62d2c030d44e1b87376742883987707
Mauricio Santos @omausantos <mauwebjc@gmail.com>
2022-05-09 22:33:31 -0300

    [A1] Componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { YesNoButtonGroupModule } from './shared/components/yes-no-button-group/yes-no-button-group.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    YesNoButtonGroupModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
